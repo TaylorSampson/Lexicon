a6d7934da4b08ce2cca07fd29efcd07a70e15055
Taylor Sampson <taylorsampsondesign@gmail.com>
2015-01-20 02:07:05 -0600

    1.20.15
    Origin

---
+++ www/js/main.js
@@ -0,0 +1,14 @@
+// var client = require("swagger-client")
+//
+// var swagger = new client.SwaggerApi({
+//   url: 'http://petstore.swagger.wordnik.com/api/api-docs',
+//   success: function() {
+//     if(swagger.ready === true) {
+//       swagger.apis.pet.getPetById({petId:1});
+//     }
+//   }
+// });
+
+// function tester(){
+// document.getElementById('test').innerHTML = "SHIT";
+// }
