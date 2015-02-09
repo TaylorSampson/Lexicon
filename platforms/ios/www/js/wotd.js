fec617e1d8e915d127e30a3d71704a703d25ea39
Taylor Sampson <taylorsampsondesign@gmail.com>
2015-02-08 22:53:22 -0600

    2.8.15
    Splash And icon updated.

--- platforms/ios/www/js/wotd.js
+++ platforms/ios/www/js/wotd.js
@@ -1,5 +1,7 @@
 
     (function() {
+
+
         var wotdAPI = "http://api.wordnik.com:80/v4/words.json/wordOfTheDay?api_key=18dac42cc58520dbc60050b320b09a6b37c83226914b21ce0";
         $.getJSON( wotdAPI, function(returneddata) {
           var wotdActual = returneddata.word;
@@ -16,23 +18,19 @@
           $.getJSON( pronounceAPI, function(pronouncedata){
             $("#wotdPronounce").html(pronouncedata[0].raw);
           });
+
+          
           var wotdAudioAPI = "http://api.wordnik.com:80/v4/word.json/"+proWOTD+"/audio?useCanonical=false&limit=50&api_key=18dac42cc58520dbc60050b320b09a6b37c83226914b21ce0";
           $.getJSON(wotdAudioAPI, function(wotdsoundData){
             var wotdaudioplayer = document.getElementById('wotdplayer');
             wotdaudioplayer.src = wotdsoundData[0].fileUrl;
             wotdaudioplayer.load();
-
           });
           $("#def2").css({"border-bottom": "1px solid #ffcaca"});
-
           $('#ex').append(returneddata.examples[0].text);
           $("#example").append("example:");
           $('#defpos2').append(returneddata.definitions[1].partOfSpeech);
           $('#def2').append(returneddata.definitions[1].text);
           $("#tag2").append("definition:");
-
-
-
-
         });
       })();
